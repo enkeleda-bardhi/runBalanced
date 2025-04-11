682013f0f5fcb9a2d9f340b373a4068356bcf982
benedetta-c <corso.benedetta@gmail.com>
2025-04-11 09:24:38 +0200

    added controllers

--- run_balanced/lib/screens/login.dart
+++ run_balanced/lib/screens/login.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
-const double kDefaultPadding = 26.0;
+
+const double kDefaultPadding = 16.0;
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Login Run Balanced', // Primary title
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Welcome back!', // Secondary title
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+        centerTitle: true, // Ensures the titles are centered
       ),
-      body: Padding(
+      body: SafeArea(
+        child: Padding(
         padding: const EdgeInsets.all(kDefaultPadding),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
                 border: OutlineInputBorder(),
@@ -25,6 +44,7 @@ class LoginPage extends StatelessWidget {
             ),
             SizedBox(height: kDefaultPadding),
             TextField(
+              controller: passwordController,
               decoration: InputDecoration(
                 labelText: 'Password',
                 border: OutlineInputBorder(),
@@ -40,7 +60,8 @@ class LoginPage extends StatelessWidget {
             ),
           ],
         ),
-      ),
+      ), 
+      )
     );
   }
 }
