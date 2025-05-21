688f9b2799790bd25e8962cadf579cf1f61a14d3
Vincent Leone <vincent.leone@studenti.unipd.it>
2025-05-21 09:43:07 +0200

    progress bar
    progress bar update

--- run_balanced/lib/widgets/progress_bar_widget.dart
+++ run_balanced/lib/widgets/progress_bar_widget.dart
@@ -4,27 +4,45 @@ class ProgressBarWidget extends StatelessWidget {
   final String label;
   final double value; // da 0 a 100
 
-  const ProgressBarWidget({required this.label, required this.value});
+  const ProgressBarWidget({required this.label, required this.value, super.key});
 
   Color getColor(double val) {
-    if (val < 60) return Colors.red;
-    if (val < 80) return Colors.orange;
+    if (val < 20) return Colors.red;
+    if (val < 40) return Colors.orange;
+    if (val < 60) return Colors.yellow;
+    if (val < 80) return Colors.lightGreen;
     return Colors.green;
   }
 
   @override
   Widget build(BuildContext context) {
+    final color = getColor(value);
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(label),
-        SizedBox(height: 5),
-        LinearProgressIndicator(
-          value: value / 100,
-          valueColor: AlwaysStoppedAnimation(getColor(value)),
-          backgroundColor: Colors.grey.shade300,
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
         ),
-        SizedBox(height: 10),
+        const SizedBox(height: 6),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: LinearProgressIndicator(
+            value: value / 100,
+            minHeight: 16,
+            backgroundColor: Colors.grey.shade300,
+            valueColor: AlwaysStoppedAnimation<Color>(color),
+          ),
+        ),
+        const SizedBox(height: 6),
+        Text(
+          '${value.toStringAsFixed(0)}%',
+          style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+        ),
       ],
     );
   }
