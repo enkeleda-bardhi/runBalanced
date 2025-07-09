29059586d018611ab4388d134e2de921b40c011e
benedetta-c <corso.benedetta@gmail.com>
2025-07-09 12:19:23 +0200

    fix issues in screen

--- run_balanced/lib/screens/fatigue_detail_screen.dart
+++ run_balanced/lib/screens/fatigue_detail_screen.dart
@@ -6,12 +6,12 @@ class FatigueDetailScreen extends StatelessWidget {
   final String title;
   final List<double> data;
 
-  const FatigueDetailScreen({required this.title, required this.data});
+  const FatigueDetailScreen({super.key, required this.title, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Affaticamento $title")),
+      appBar: AppBar(title: Text("Fatigue $title")),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: FatigueChartWidget(title: title, data: data),
